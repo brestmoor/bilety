cde86668b9d9a0685198ef9f186a3ccde1fe492a
Filip Turczynowicz-Suszycki <filip.turczynowicz-suszycki@pl.abb.com>
2018-01-22 08:41:57 +0100

    alert success

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -12,7 +12,7 @@ class AccountsController < ApplicationController
    @account.balance += recharge.to_f
    if @account.save
      flash[:komunikat] = 'Your account was recharged.'
-     flash[:class] = 'alert alert-danger'
+     flash[:class] = 'alert alert-success'
      redirect_to '/accounts/edit'
    end
  end
